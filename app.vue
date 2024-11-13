517405808f77823f67d7863974c31c21a5b2299d
iamsmthing <git@stackblitz.com>
2024-11-13 12:19:02 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Product
+      imageSrc="http://images.ctfassets.net/n58cc9djl3c5/5K3LkcTpxTCkSaZqgrVxXY/09deb907562a0e93637aaddccbc94ada/SmokeDetector.jpg?fm=jpg&fl=progressive"
+      title="How do you test a carbon monoxide detector?"
+      link="/your-article-url"
+    />
+  </div>
+</template>
+<script setup>
+import ImageCard from '~/components/Product.vue';
+</script>
---
+++ components/Product.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="imageSrc" class="card-img-top" alt="Card image" />
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <a :href="link" class="btn btn-link">Read More →</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.card-title {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+.btn-link {
+  color: #007bff;
+  text-decoration: none;
+}
+.btn-link:hover {
+  text-decoration: underline;
+}
+</style>
